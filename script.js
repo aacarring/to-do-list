4bc280a690613a35527fed24800122bdb2738128
Alexa Carrington <acarring@wellesley.edu>
2020-11-10 11:48:15 -0500

    Added addToDo() function in script.js

--- script.js
+++ script.js
@@ -4,5 +4,30 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 //Event listeners
+todoButton.addEventListener('click', addTodo);
 
 //Functions
+function addTodo(event) {
+    //Prevent form from submitting
+    event.preventDefault();
+    //To-do div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    //Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    //Append to list
+    todoList.appendChild(todoDiv);
+};
